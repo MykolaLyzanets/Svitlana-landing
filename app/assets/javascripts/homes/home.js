ad4a64ea1a8c5ab8bf7d2c7b7332f2e43874cd34
work <olenevo007@gmail.com>
2023-07-21 21:54:08 +0300

    add new code

---
+++ app/assets/javascripts/homes/home.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $(".nav-link").click(function(e) {
+    if ($(window).width() < 992) {
+      e.preventDefault();
+      var target = $(this).attr("href");
+      $(target).collapse('toggle');
+    }
+  });
+});
+
+const navLinks = document.querySelectorAll('.navbar-nav a');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    const headerOffset = 50; // Висота фіксованого заголовка, якщо він є
+    const elementPosition = target.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - headerOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth',
+  });
+  });
+});
+
+
+
